868ce72666c622b745c4f09e94972ef59e50472c
Eduardo Díaz <iamedu@gmail.com>
2011-04-20 18:43:26 -0500

    We have now our base project

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source :rubygems
 
 gem "rack-flash"
 gem "sinatra"
+gem "sinatra-reloader"
 gem "haml"
 gem "sass"
 
--- app.rb
+++ app.rb
@@ -1,7 +1,15 @@
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'haml'
+require 'sass'
 
 get '/' do
-    "Hello world!"
+    haml :index
+end
+
+get '/stylesheets/*' do
+    content_type 'text/css'
+    sass '../styles/'.concat(params[:splat].join.chomp('.css')).to_sym
 end
 
