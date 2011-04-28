f83ea1f8eddaf32e208eaa0f7a99c8ca9f867f3d
Eduardo Díaz <iamedu@gmail.com>
2011-04-28 05:20:23 -0500

    Fixing path

--- app.rb
+++ app.rb
@@ -56,7 +56,7 @@ get '/stylesheets/*' do
     sass '../styles/'.concat(params[:splat].join.chomp('.css')).to_sym
 end
 
-post 'token' do
+post '/token' do
     @helper = Rpx::RpxHelper.new('0573d1252dde12c6f576c550e0d3ad5f63f08a22',
                                  'rpxnow.com',
                                  'sgcarrera')
