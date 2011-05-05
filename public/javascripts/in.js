b813cbbd35f838b473a4f527c17c74a8f5abd4d0
Eduardo Díaz <iamedu@gmail.com>
2011-05-05 06:50:20 -0500

    Little changes to IN JS

--- public/javascripts/in.js
+++ public/javascripts/in.js
@@ -2,9 +2,9 @@ function loadData() {
     IN.API.Profile("me")
           .fields(["headline","summary","mainAddress"])
           .result(function(result) {
+        alert(JSON.stringify(result));
         profile = result.values[0];
         $("#title").val(profile.headline);
         $("#summary").val(profile.summary);
-        alert(JSON.stringify(profile));
     });
 }
