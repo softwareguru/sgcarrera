1e176de746db1704bcd43d7ed74319ad5041374a
Eduardo Díaz <iamedu@gmail.com>
2011-05-05 06:38:38 -0500

    Little changes to IN JS

--- public/javascripts/in.js
+++ public/javascripts/in.js
@@ -1,8 +1,6 @@
 function loadData() {
     IN.API.Profile("me")
             .result(function(result) {
-        alert("Todo bien");
-        $("#title").val(result.headline);
-        alert("Todo bien 2");
+        alert("Todo bien " + result);
     });
 }
