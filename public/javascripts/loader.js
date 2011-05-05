4450019e0e4862383557471333235560822fd37c
Eduardo Díaz <iamedu@gmail.com>
2011-05-05 06:04:46 -0500

    Adding some changes

---
+++ public/javascripts/loader.js
@@ -0,0 +1,10 @@
+google.load("jquery", "1.5.2");
+google.load("jqueryui", "1.8.12");
+google.load("webfont", "1.0.19");
+google.setOnLoadCallback(function() {
+    WebFont.load({
+        google: {
+                    families: [ 'Nobile' ]
+                }
+    });
+});
