a730f27dc19eee99c388152bf23d7d8a5c457667
Pedro Galvan <pedro@sg.com.mx>
2011-08-23 09:32:49 -0500

    Agregando el conf.js

---
+++ conf.js
@@ -0,0 +1,42 @@
+exports.port = process.env.PORT || 8080;
+exports.mongourl = 'mongodb://sgcarrera:m3t4lG34r@mongo.sgcarrera.dotcloud.com:7041/sgcarrera';
+exports.calais = 'qp7h5nccpe2fd5f2smnyte38'; 
+exports.session = {
+db : 'sgcarrerasession',
+host: 'mongo.sgcarrera.dotcloud.com',
+port: '7041',
+username: 'sgcarrerasession',
+password: 'm3t4lG34r'
+};
+
+exports.secret = 's0ftw4r3guru';
+
+//Linkedin conf
+exports.linkedin = {
+key: 'J956L9qfFnjMColuRUhr4cZCIV6r3dXQWEOaT6OecAq6vIikfovFTTBD9m0MK9w',
+secret: '6K2hXxnOgM99L3F5ZWR6mEDio49hmCew78KWhwwJSGxYxnLLhLnDq1NE368M6F'
+};
+
+//Linkedin conf
+exports.github = {
+id: 'fff5351c4334041394db',
+secret: '46e0f2a94fca6c0c3510895176dfc233c4215559'
+};
+
+//Twitter conf
+exports.twitter = {
+key: 'ZwDORqSIQUCSNZUj1frMQ',
+secret: 'kKhtWngFCBwh1VVZ0hJdMsGXbJlnNk0dngeHRVPTxc'
+};
+
+//Facebook conf
+exports.facebook = {
+id: '179087812147123',
+secret: '47d55a9654df637a0ab7af6d9daac5b6'
+};
+
+//Google conf
+exports.google = {
+id: '1084757502134.apps.googleusercontent.com',
+secret: '_gutCQl4I-ItjJ4SwUWEJaxB'
+};
